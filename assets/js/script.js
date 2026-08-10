31e5d67f932a1618f7b77b96be54471aa1702f76
Alfi Alfajuman <alfi.alfajuman@gmail.com>
2026-08-10 22:28:32 +0700

    coba frontend supabase

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,7 @@
 console.log('Sprint1A');
+const SUPABASE_URL = 'https://cmyxgygopwutjqzjjgsu.supabase.co';
+const SUPABASE_ANON_KEY ='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNteXhneWdvcHd1dGpxempqZ3N1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODYzNjE3MzUsImV4cCI6MjEwMTkzNzczNX0.Eh9JfMnlMXHMZvYq3BrDQ_hQ876wWzVn95S17FYKXlo';
+const _supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 // ================= FAQ =================
 
 const questions = document.querySelectorAll(".faq-question");
